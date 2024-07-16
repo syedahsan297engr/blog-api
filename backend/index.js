5d31ce8ffb6f636270925e1f7c8204b68c125c3e
Syed Ahsan <syedahsannoori@gmail.com>
2024-07-16 16:37:16 +0500

    project structure setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
